ee627963393e479189b4e60b57143bfa8574e068
Hikanyan <hikanyan0131@gmail.com>
2023-05-14 18:19:34 +0900

    [Add]GameManager

---
+++ Assets/Hikanyan_Assets/Task/Script/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hikanyan_Assets.Task.Script
+{
+    public class GameManager : AbstractSingleton<GameManager>
+    {
+        public enum GAME_STATE
+        {
+            NONE,
+            TITEL,
+            STRAT,
+            CLEAR,
+            GAMEOVER
+        }
+
+        public int Point;
+
+        public GAME_STATE GameState = GAME_STATE.NONE;
+        //[SerializeField]
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            switch (GameState)
+            {
+                case GAME_STATE.STRAT:
+                    Debug.Log(Point);
+                    break;
+            }
+        }
+    }
+}
