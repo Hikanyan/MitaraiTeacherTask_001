b7cc6a28646630d5f8f671813e99e0828ca578d3
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-05-16 17:58:35 +0900

    [Update]移動できるようになった

--- Assets/Hikanyan_Assets/ActionGame/Script/PlayerMove.cs
+++ Assets/Hikanyan_Assets/ActionGame/Script/PlayerMove.cs
@@ -6,9 +6,12 @@ namespace Hikanyan_Assets.ActionGame.Script
 {
     public class PlayerMove : MonoBehaviour
     {
+        [SerializeField] float _moveSpeed = 5.0f;
+        
         [SerializeField] private GameObject _pointer;
         private Transform _transform;
         [SerializeField] private GameObject _bullet;
+        LayerMask groundLayer;
 
         private float _timer;
         private float _count=1;
@@ -21,6 +24,7 @@ namespace Hikanyan_Assets.ActionGame.Script
 
         private void Update()
         {
+            Move();
             Pointer();
             _timer += Time.deltaTime;
             if (_timer  >= _count)
@@ -30,9 +34,21 @@ namespace Hikanyan_Assets.ActionGame.Script
             }
         }
 
+        void Move()
+        {
+            var posY = _transform.position.y;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                posY += _moveSpeed * Time.deltaTime;
+            }
+            transform.position += new Vector3(Input.GetAxisRaw("Horizontal"),posY).normalized * (_moveSpeed * Time.deltaTime);
+        }
+
+        
+
         void BulletShot()
         {
-            Instantiate(_bullet,_transform);
+            Instantiate(_bullet,new Vector3(_transform.position.x,_transform.position.y,_transform.position.z),Quaternion.identity);
         }
 
         void Pointer()
