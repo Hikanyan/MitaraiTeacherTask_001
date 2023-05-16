d299fe3243d95efd8aca9c60c32e9a0b3a7cb468
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-05-16 15:29:20 +0900

    [Add]ActionGame Player,Enemy

---
+++ Assets/Hikanyan_Assets/ActionGame/Script/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hikanyan_Assets.ActionGame.Script
+{
+    public class GameManager:AbstractSingleton<GameManager>
+    {
+        
+    }
+}
--- Assets/Hikanyan_Assets/ActionGame/Script/PlayerMove.cs
+++ Assets/Hikanyan_Assets/ActionGame/Script/PlayerMove.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+namespace Hikanyan_Assets.ActionGame.Script
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMove : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject _pointer;
+        private Transform _transform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            _transform = this.transform;
+            _pointer = Instantiate(_pointer, new Vector3(this.transform.position.x, this.transform.position.y, 10),
+                Quaternion.identity, _transform);
+        }
+
+        private void Update()
+        {
+            Pointer();
+        }
+
+        void BulletShot()
+        {
+            
+        }
+
+        void Pointer()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10;
+            Vector3 target = Camera.main.ScreenToWorldPoint(mousePosition);
+            _pointer.transform.position = target;
+        }
     }
 }
